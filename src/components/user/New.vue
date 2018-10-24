bbe69762546ab8884ff674d63cafe31113f23766
Ralf Wilke <wilke@ihf.rwth-aachen.de>
2018-10-24 11:47:07 +0200

    Added Read only display of Status flags and user's roles of no user-change-rights are granted

--- src/components/user/New.vue
+++ src/components/user/New.vue
@@ -39,6 +39,7 @@
 								<input type="text" v-model="form.email" class="form-control">
 							</div>
 						</div>
+						<!-- if user is allowed to change a rolem display selection box-->
 						<div class="form-group" v-if="this.$store.getters.permission('user.change_role')">
 							<label class="col-lg-2 control-label">{{ $t('users.general.roles') }}</label>
 							<div class="col-lg-10">
@@ -47,18 +48,35 @@
 														placeholder="Type to search"></multiselect>
 							</div>
 						</div>
+						<!-- if user is not allowed to change a role, just display it -->
+						<div class="form-group" v-if="!this.$store.getters.permission('user.change_role')">
+							<label class="col-lg-2 control-label">{{ $t('users.general.roles') }}</label>
+							<div class="col-lg-10">
+								<span v-for="(role, index) in form.roles" v-bind:key="`role-${index}`" class="label label-roles">
+									{{ role }}
+								</span>
+							</div>
+						</div>
 						<div class="form-group">
 							<label class="col-lg-2 control-label">{{ $t('users.general.othersettings') }}</label>
 							<div class="col-lg-10">
 								<table style="width: 100%">
 									<tbody style="text-align: center">
 									<tr>
-										<td>Enabled</td>
-										<td>Email Valid</td>
+										<td>{{ $t('users.general.enabled') }}</td>
+										<td>{{ $t('users.general.email_valid') }}</td>
 									</tr>
 									<tr>
-										<td><input type="checkbox" v-model="form.enabled" class="userEnabeldCheckBox" value="true" checked="checked"></td>
-										<td><input type="checkbox" v-model="form.email_valid" class="userEmailValidCheckBox" value="true" checked="checked"></td>
+										<td>
+											<input v-if="this.$store.getters.permission('user.change_role')" type="checkbox" v-model="form.enabled" class="userEnabeldCheckBox" value="true" checked="checked">
+											<span v-if="form.enabled && !this.$store.getters.permission('user.change_role')" class="label label-success">{{ $t('general.yes') }}</span>
+											<span v-if="!form.enabled && !this.$store.getters.permission('user.change_role')" class="label label-primary">{{ $t('general.no') }}</span>
+										</td>
+										<td>
+											<input v-if="this.$store.getters.permission('user.change_role')" type="checkbox" v-model="form.email_valid" class="userEnabeldCheckBox" value="true" checked="checked">
+											<span v-if="form.email_valid && !this.$store.getters.permission('user.change_role')" class="label label-success">{{ $t('general.yes') }}</span>
+											<span v-if="!form.email_valid && !this.$store.getters.permission('user.change_role')" class="label label-primary">{{ $t('general.no') }}</span>
+										</td>
 									</tr>
 									</tbody>
 								</table>
@@ -88,19 +106,23 @@
 			axios.get('auth/users/roles')
 				.then(response => {
 					this.formData.roles = response.data;
+				}).catch(e => {
+					console.log('Error getting user\'s roles in user/new.vue');
 				});
 
 			// load data of given id
 			if (this.$route.params.id) {
 				axios.get('users/' + this.$route.params.id).then(response => {
 					this.editing = true;
-					this.form._id = response.body._id;
-					this.form._rev = response.body._rev;
-					this.form.email = response.body.email;
-					this.form.roles = response.body.roles;
-					this.form.enabled = response.body.enabled;
-					this.form.email_valid = response.body.email_valid;
+					this.form._id = response.data._id;
+					this.form._rev = response.data._rev;
+					this.form.email = response.data.email;
+					this.form.roles = response.data.roles;
+					this.form.enabled = response.data.enabled;
+					this.form.email_valid = response.data.email_valid;
+					console.log(this.form.roles);
 				}).catch(e => {
+					console.log('Error getting user\'s individual details with axios');
 					this.$router.push('/users');
 				});
 			}
@@ -108,6 +130,7 @@
 		data() {
 			return {
 				editing: false,
+				messages: ['hello', 'vue', 'test'],
 				form: {
 					_id: '',
 					password: '',
@@ -168,4 +191,9 @@
 
 <style scoped>
 
+	.label-roles {
+		font-size: small;
+		margin-right: 2px;
+		background-color: #469408;
+	}
 </style>
