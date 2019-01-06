03920bfa861c188f10fa878099252fe066785c09
Ralf Wilke <dh3wr@darc.de>
2019-01-06 23:28:46 +0100

    Added home page carousel

--- src/Home.vue
+++ src/Home.vue
@@ -1,15 +1,49 @@
 <template>
     <v-container>
-        <v-layout
-                text-xs-center
-                wrap
-        >
-            <v-flex mb-4>
-                <h1 class="display-2 font-weight-bold mb-3">
-                    {{ $t('navigation.home') }}
-                </h1>
-            </v-flex>
-        </v-layout>
+		<div id="header-carousel">
+			<v-carousel
+				hide-delimiters
+				height="400"
+			>
+				<v-carousel-item v-for="(index,i) in 10" :key="i">
+					<v-layout>
+						<v-flex xs7>
+							<v-card
+								color="grey"
+								class="flexcard"
+							>
+								<v-card-title primary-title>
+									<div>
+										<h3 class="headline mb-0">{{ $t('home.carousel.' + index + '.title') }}</h3>
+
+									</div>
+								</v-card-title>
+								<v-card-text class="grow">
+									{{ $t('home.carousel.' + index + '.text') }}
+								</v-card-text>
+								<v-card-actions v-if="getLink(index) && getLink(index) !== 'NONE'">
+									<a :href="getLink(index)" target="_blank">
+										<v-btn large color="primary">
+											External Link with more information
+										</v-btn>
+									</a>
+								</v-card-actions>
+							</v-card>
+						</v-flex>
+						<v-flex xs5 v-if="getLink(index) && getLink(index) !== 'NONE'">
+							<a :href="getLink(index)" target="_blank">
+									<v-img :aspect-ratio="400/400" :src="'./img/carousel/img' + index + '.png'">
+									</v-img>
+							</a>
+						</v-flex>
+						<v-flex xs5 v-else>
+							<v-img :aspect-ratio="400/400" :src="'./img/carousel/img' + index + '.png'">
+							</v-img>
+						</v-flex>
+					</v-layout>
+				</v-carousel-item>
+			</v-carousel>
+		</div>
 
     </v-container>
 </template>
@@ -21,12 +55,18 @@
 		methods: {
 			sendEvent() {
 				this.$root.$emit('ReloadSidebarIcons');
+			},
+			getLink(index) {
+				return this.$i18n.t('home.carousel.' + index + '.link');
 			}
 		}
-	}
+	};
 
 </script>
 
 <style>
-
+	.flexcard {
+		display: flex;
+		flex-direction: column;
+	}
 </style>
