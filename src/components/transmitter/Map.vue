b201898c9f3fc0d57b4fc2a83a0536c4d8d1d765
Ralf Wilke <dh3wr@darc.de>
2019-02-04 00:33:23 +0100

    Rework of data structure of Map.vue, not finished

--- src/components/transmitter/Map.vue
+++ src/components/transmitter/Map.vue
@@ -4,8 +4,6 @@
 			<v-card-title>
 				<span class="headline">{{ $t('transmitters.map.title') }}</span>
 			</v-card-title>
-			<v-card-text>
-			</v-card-text>
 			<!--OSM Map-->
 			<v-card-media>
 				<l-map
@@ -20,12 +18,11 @@
 					>
 					</l-tile-layer>
 					<l-marker
-						v-for="(item, index) in map.markers"
+						v-for="(item, markerindex) in map.markers"
 						:key="item.name"
 						:lat-lng="item.coordinates"
 						:icon="item.icon"
 						@click="loadCoverage(item.id)"
-						@mouseover="loadTransmitterDetails(item.id, index)"
 					>
 						<l-popup v-if="item.details !== undefined">
 							{{ item.details.changed_on }}
@@ -35,18 +32,36 @@
 							v-if="item.details"
 							style="width: 400px;"
 						>
-							<v-layout fluid min-width="400px">
-								<v-card flat>
+							<v-layout fluid>
+								<v-card flat color="green">
 									<v-card-title class="headline">
-										{{ item.id }}
+										<v-layout align-center justify-space-between row>
+											<v-flex xs2>{{ item.id }}</v-flex>
+											<v-spacer></v-spacer>
+											<v-flex xs3>
+												<span class="align-end">
+													<!--
+													<div v-if="item.details.status.online" class="chartContainer">
+
+														<chart-message-queue
+															:chartData="chartDataMessageQueue(item.name)"
+															style="width: 100px; height: 70px;"
+														>
+														</chart-message-queue>
+													</div>
+													-->
+												</span>
+											</v-flex>
+										</v-layout>
 									</v-card-title>
 									<v-card-text>
-										<v-layout wrap>
+										<div style="width: 350px;">
+										<v-layout row wrap>
 											<v-flex xs5>{{ $t('general.transmitter_groups') }}</v-flex>
 											<v-flex xs7>
 												<v-chip
-													v-for="(group, index) in item.details.groups"
-													:key="index"
+													v-for="(group, groupindex) in item.details.groups"
+													:key="groupindex"
 													small
 													label
 													color="primary"
@@ -54,11 +69,13 @@
 													{{ group }}
 												</v-chip>
 											</v-flex>
+										</v-layout>
+										<v-layout wrap>
 											<v-flex xs5>{{ $t('general.owners') }}</v-flex>
 											<v-flex xs7>
 												<v-chip
-													v-for="(owner, index) in item.details.owners"
-													:key="index"
+													v-for="(owner, ownerindex) in item.details.owners"
+													:key="ownerindex"
 													small
 													label
 													color="primary"
@@ -91,7 +108,7 @@
 											<v-flex xs7>{{ getTimestampFormated(item.details.changed_on) }}
 												{{ $t('general.byUser') }} {{ item.details.changed_by }}
 											</v-flex>
-										</v-layout>
+										</v-layout></div>
 									</v-card-text>
 								</v-card>
 							</v-layout>
@@ -99,6 +116,31 @@
 					</l-marker>
 				</l-map>
 			</v-card-media>
+			<v-card-text>
+				<v-layout wrap row>
+					<v-flex xs3>
+						<v-checkbox
+							v-model="checkbox.pttstatus"
+							:label="$t('transmitters.map.checkbox.showpttstatus')"
+						>
+						</v-checkbox>
+					</v-flex>
+					<v-flex xs3>
+						<v-checkbox
+							v-model="checkbox.onlineonly"
+							:label="$t('transmitters.map.checkbox.showonlineonly')"
+						>
+						</v-checkbox>
+					</v-flex>
+					<v-flex xs3>
+						<v-checkbox
+							v-model="checkbox.widerangeonly"
+							:label="$t('transmitters.map.checkbox.showwiderangeonly')"
+						>
+						</v-checkbox>
+					</v-flex>
+				</v-layout>
+			</v-card-text>
 		</v-card>
 	</v-container>
 </template>
@@ -116,31 +158,43 @@
 			this.loadNodes();
 		},
 		mounted() {
+			// Handler for Map Moved
 			this.$refs.map.mapObject.on('moveend', () => {
 				this.centerLatLong = this.$refs.map.mapObject.getCenter();
 				this.map.bounds = this.$refs.map.mapObject.getBounds();
 				console.log('map was moved');
-				this.loadTransmitterDetailsInBound();
+				this.loadMissingTransmitterDetailsInBound();
+				this.updateWSConnections();
+				this.updateMarkersOnMapafterMove();
 			});
+			// Initialize zoom
 			this.map.zoom = this.$store.getters.map.zoom;
 		},
+
+		/*
 		watch: {
 			'isLoadingData.transmitters'() {
 				if (!this.isLoadingData.transmitters) {
 					this.updateMapContent();
 				}
 			}
-/*
+
 			,
 			'isLoadingData.nodes'() {
 				if (!this.isLoadingData.nodes) {
 					this.updateMapContent();
 				}
 			}
-*/
+
 		},
+*/
 		data() {
 			return {
+				checkbox: {
+					pttstatus: false,
+					onlineonly: false,
+					widerangeonly: false
+				},
 				centerLatLong: null,
 				isLoadingData: {
 					transmitters: true,
@@ -157,76 +211,183 @@
 					center: [50, 6],
 					bounds: null
 				},
-				data: {
+				staticData: {
 					transmitters: {},
 					nodes: {}
 				},
-				icons: {}
+				monitoringData: {
+					transmitters: {},
+					nodes: {}
+				},
+				icons: {},
+				wsHandler: {}
 			};
 		},
 		computed: {
 		},
 		methods: {
+			chartDataMessageQueue(transmittername) {
+				if (this.transmitterrows[transmitterindex] &&
+					'status' in this.transmitterrows[transmitterindex] &&
+					'messages' in this.transmitterrows[transmitterindex].status &&
+					'queued' in this.transmitterrows[transmitterindex].status.messages) {
+					return {
+						labels: ['L', '', 'M', '', 'H'],
+						datasets: [{
+							backgroundColor: ['#469408', '#e0d32b', '#e08b27', '#e04530', '#d9230f'],
+							data: this.transmitterrows[transmitterindex].status.messages.queued
+						}]
+					};
+				} else {
+					return {
+						labels: ['E', 'M', 'P', 'T', 'Y'],
+						datasets: [{
+							backgroundColor: ['#469408', '#e0d32b', '#e08b27', '#e04530', '#d9230f'],
+							data: [0, 0, 0, 0, 0]
+						}]
+					};
+				}
+			},
 			getTimestampFormated(ISOTime) {
 				return moment(ISOTime).format('LLL');
 			},
 			loadCoverage() {
 			},
-			updateMapContent() {
-				console.log('updateMapContent');
+			skipthisTransmitter(transmittername) {
+				if (this.checkbox.widerangeonly && this.staticData.transmitters[transmittername].usage === 'widerange') {
+					return (this.checkbox.onlineonly && (transmittername in this.monitoringData.transmitters));
+				} else {
+					return false;
+				}
+			},
+			transmitterInBounds(transmittername) {
+				if (transmittername in this.staticData.transmitters &&
+				'coordinates' in this.staticData.transmitters[transmittername]) {
+					let bounds = this.$refs.map.mapObject.getBounds();
+					return bounds.contains(new L.LatLng(this.staticData.transmitters[transmittername].coordinates[0],
+						this.staticData.transmitters[transmittername].coordinates[1]));
+				} else {
+					console.log('Transmitter ' + transmittername + ' not found in staticData or coodinated not present');
+					return false;
+				}
+			},
+			updateMarkersOnMapafterMove() {
+				console.log('updateMarkersonMap executed');
 				let markerTransmitters = [];
 				let polylineTransmitters = [];
-
-				for (let i = 0; i < this.data.transmitters.rows.length; i++) {
-					// if (this.skipThisTransmitter(transmitter)) return true;
-					let transmitter = this.data.transmitters.rows[i];
-					let selectedMarkerIcon = null;
-					// find icon
-					if (transmitter.value.usage === 'widerange') {
-						// TODO: There is no information about the online or offline status in the transmitter/map answer yet
-						// so for now all transmitters are displayed as online
-						selectedMarkerIcon = this.icons.iconTransmitterWiderangeOnline;
-					} else {
-						// TODO: There is no information about the online or offline status in the transmitter/map answer yet
-						// so for now all transmitters are displayed as online
-						selectedMarkerIcon = this.icons.iconTransmitterPersonalOnline;
+				// Only put transmitters inside of bound on map
+				for (let transmitterID in this.staticData.transmitters) {
+					if (this.transmitterInBounds(transmitterID)) {
+						if (!this.checkbox.pttstatus) {
+							// Static Display
+							if (!this.skipthisTransmitter(transmitterID)) {
+								markerTransmitters.push({
+									id: transmitterID,
+									name: 't_' + transmitterID,
+									coordinates: this.staticData.transmitters[transmitterID].coordinates,
+									icon: this.getCorrespondingStaticIcon(transmitterID)
+								});
+							}
+						} else {
+							// OnAir Display
+						}
 					}
-					markerTransmitters.push({
-						id: transmitter.id,
-						name: 't_' + transmitter.id,
-						coordinates: transmitter.value.coordinates,
-						icon: selectedMarkerIcon
-					});
 				}
 				this.map.markers = markerTransmitters;
 				console.log(this.map.markers);
-				this.loadTransmitterDetailsInBound();
 			},
-			loadTransmitterDetailsInBound() {
-				let bounds = this.$refs.map.mapObject.getBounds();
-				for (let i = 0; i < this.map.markers.length; i++) {
-					if (bounds.contains(new L.LatLng(this.map.markers[i].coordinates[0], this.map.markers[i].coordinates[1]))) {
-						// console.log('IN:  ' + this.map.markers[i].id);
-						this.loadTransmitterDetails(this.map.markers[i].id, i);
+			loadMissingTransmitterDetailsInBound() {
+				for (let transmitterID in this.staticData.transmitters) {
+					if (this.transmitterInBounds(transmitterID)) {
+						this.loadMissingTransmitterDetail(transmitterID);
 					} else {
 						// console.log('OUT: ' + this.map.markers[i].id);
 					}
 				}
 			},
-			loadTransmitterDetails(transmittername, markerindex) {
-				if (!('details' in this.map.markers[markerindex])) {
+			updateWSConnections() {
+				for (let transmitterID in this.staticData.transmitters) {
+					if (this.transmitterInBounds(transmitterID)) {
+						// Transmitter marker is on Map
+						this.addWShandler(transmitterID);
+					} else {
+						this.removeWShandler(transmitterID);
+					}
+				}
+			},
+			removeWShandler(transmittername) {
+				//console.log('try removeWS TX: ' + transmittername);
+				if (transmittername in this.wsHandler) {
+					console.log('removeWS TX: ' + transmittername);
+					this.wsHandler[transmittername].close();
+					delete this.wsHandler[transmittername];
+					if (transmittername in this.monitoringData.transmitters) {
+						delete this.monitoringData.transmitters[transmittername];
+					}
+				}
+			},
+			addWShandler(transmittername) {
+				//console.log('try AddWS TX: ' + transmittername);
+				if (!(transmittername in this.wsHandler)) {
+					console.log('newWS TX: ' + transmittername);
+					this.wsHandler[transmittername] = new WebSocket(this.$store.getters.url.telemetry +
+						'/telemetry/transmitters/' + transmittername);
+					this.wsHandler[transmittername].addEventListener('message', e => {
+						let data = JSON.parse(e.data);
+						if (!this.$helpers.isEmpty(data)) {
+							console.log(transmittername);
+							console.log(data);
+							if (!(transmittername in this.monitoringData.transmitters)) {
+								// save initial copy of data
+								this.monitoringData.transmitters[transmittername] = data;
+							} else {
+								// overwrite with new chunk of data
+								this.monitoringData.transmitters[transmittername] =
+									Object.assign(this.monitoringData.transmitters[transmittername], data);
+							}
+						} else {
+							delete this.monitoringData.transmitters[transmittername];
+						}
+					});
+				}
+			},
+			loadMissingTransmitterDetail(transmittername) {
+				// If transmitter is not already present or details not already loaded (by existens of _rev)
+				if ((!(transmittername in this.staticData.transmitters)) ||
+					(!('_rev' in this.staticData.transmitters[transmittername]))) {
 					// Load transmitter details
 					console.log('Loading TX details of: ' + transmittername);
 					this.isLoadingData.transmitterdetails = true;
 					this.$axios.get('transmitters/' + transmittername)
 						.then(response => {
-							let updatedmarker = this.map.markers[markerindex]
-							updatedmarker.details = response.data;
-							this.map.markers.splice(markerindex, 1, updatedmarker);
+							this.staticData.transmitters[transmittername] = response.data;
+							delete this.staticData.transmitters[transmittername]._id;
 							this.isLoadingData.transmitterdetails = false;
 						}).catch(e => {
 							console.log('Error getting transmitter details in transmitters/Map.vue');
 					});
+				}
+			},
+			getCorrespondingStaticIcon(transmittername) {
+				// find icon
+				if (this.staticData.transmitters[transmittername].usage === 'widerange') {
+					// Widerange
+					if (transmittername in this.monitoringData) {
+						// Widerange and online
+						return this.icons.iconTransmitterWiderangeOnline;
+					} else {
+						// Widerange and offline
+						return this.icons.iconTransmitterWiderangeOffline;
+					}
+				} else {
+					// Personal
+					if (transmittername in this.monitoringData) {
+						// Personal and online
+						return this.icons.iconTransmitterPersonalOnline;
+					} else {
+						// Peronal and offline
+						this.icons.iconTransmitterPersonalOffline;
+					}
 				}
 			},
 			createIcons() {
@@ -279,8 +440,15 @@
 				this.isLoadingData.transmitters = true;
 				this.$axios.get('transmitters/map')
 					.then(response => {
-						this.data.transmitters = response.data;
+						for (let i = 0; i < response.data.rows.length; i++)
+						{
+							this.staticData.transmitters[response.data.rows[i].id] =  {
+									'usage': response.data.rows[i].value.usage,
+									'coordinates': response.data.rows[i].value.coordinates
+								};
+						}
 						this.isLoadingData.transmitters = false;
+
 					})
 					.catch(e => {
 						console.log('Error getting transmitter locations in transmitters/Map.vue');
@@ -291,7 +459,10 @@
 				this.isLoadingData.nodes = true;
 				this.$axios.get('nodes')
 					.then(response => {
-						this.data.nodes = response.data;
+						for (let i = 0; i < response.data.rows.length; i++) {
+							this.staticData.nodes[response.data.rows[i]._id] = response.data.rows[i];
+							delete this.staticData.nodes[response.data.rows[i]._id]._id;
+						}
 						this.isLoadingData.nodes = false;
 					})
 					.catch(e => {
@@ -305,5 +476,8 @@
 </script>
 
 <style scoped>
-
+	.chartContainer {
+		border: 1px solid lightgrey;
+		box-sizing: border-box;
+	}
 </style>
