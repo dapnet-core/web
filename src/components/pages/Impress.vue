b0dd38d19e8af4435b9ee402a6bc7b9f4388a82e
Ralf Wilke <dh3wr@darc.de>
2019-10-09 23:44:49 +0200

    Added impress and privacy pages

---
+++ src/components/pages/Impress.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-card>
+		<v-card-text>
+			<span class="headline">{{ $t('impress.title') }}</span><br><br>
+			<span class="title">Angaben gemäß § 5 TMG:</span><br>
+
+			Ralf Wilke<br>
+			Amateurfunkgruppe an der RWTH Aachen<br>
+			Institut für Hochfrequenztechnik<br>
+			52074 Aachen<br><br>
+
+			<span class="title">Kontakt:</span><br>
+			Telefon: 	+49 (0) 241 80 27959<br>
+			E-Mail: 	support@hampager.de<br>
+			Webseite: 	www.afu.rwth-aachen.de<br><br>
+
+			<span class="title">Haftungsausschluss (Disclaimer)</span><br><br>
+			<span class="subtitle-2">Haftung für Inhalte</span><br>
+
+			Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+			Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+			übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen,
+			die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+			Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist
+			jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
+			entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br><br>
+
+			<span class="subtitle-2">Haftung für Links</span><br>
+
+			Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
+			Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
+			Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+			wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
+			Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist
+			jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
+			Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br><br>
+
+			<span class="subtitle-2">Urheberrecht</span><br>
+
+			Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
+			Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+			Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+			Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+			Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter
+			beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+			Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden
+			von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br><br>
+
+			Quelle: http://www.e-recht24.de
+			<script type="application/ld+json">
+				{
+					"@context": "http://schema.org",
+					"@type": "Organization",
+					"url": "https://www.afu.rwth-aachen.de",
+					"contactPoint": [{
+						"@type": "ContactPoint",
+						"telephone": "+49-241-80-27959",
+						"contactType": "technical support"
+					}]
+				}
+			</script>
+		</v-card-text>
+	</v-card>
+</template>
+
+<script>
+    export default {
+        name: "impress"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/pages/Privacy.vue
@@ -0,0 +1,57 @@
+<template>
+	<v-card>
+		<v-card-text>
+			<span class="headline">{{ $t('privacy.title') }}</span><br><br>
+			<span class="title">Datenschutz</span><br>
+			<p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre
+				personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie
+				dieser Datenschutzerklärung.
+			</p>
+			<p>Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf
+				unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift, Amateurfunk-Rufzeichen oder
+				E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten
+				werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. Davon ausgeschlossen ist die
+				Verteilung der Daten innerhalb des Netzwerkes zu seinem bestimmungsgemäßen Betrieb.
+			</p>
+			<p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail)
+				Sicherheitslücken aufweisen kann. Insbesondere im HAMNET erfolgt die Datenübertragung auch Ihrer
+				personenbezogenen Daten von Gesetzes Wegen in offener Sprache, also ohne Verschlüsselung. Ein
+				lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. Sie erklären sich dazu
+				ausdrücklich einverstanden.
+			</p><br>
+
+			<span class="title">
+				Besondere Erklärung im Rahmen der Verordnung (EU) 2016/679 (Datenschutz-Grundverordnung)
+			</span><br>
+			<p>Durch die Teilnahme am durch diese Seite bereitgestellten Funkruf-Netzwerkes geben Sie ein freiwilliges
+				Einverständnis, dass Ihre personenbezogenen Daten und insb. auch die von Ihnen verschickten Funkrufe
+				gespeichert, zum bestimmungsgemäßen Betrieb verarbeitet und innerhalb des Netzwerkes DAPNET geteilt
+				werden. Sie können jederzeit eine Information über die von Ihnen gespeicherten Daten, deren Korrektur
+				oder Löschung verlangen. Dies bitte über das
+				<a href="https://support.hampager.de">Support-Ticket-System</a> verlangen.
+			</p>
+			<p>Insb. geben Sie obenstehende freiwilligen Einwilligungen durch das Absenden einer Anfrage im
+				Ticket-System zur Erteilung eines Benutzerzugangs oder eines Zugangs eines Senders.
+			</p><br>
+
+			<span class="title">Besondere Benutzung der Daten eines teilnehmenden Senders</span><br>
+			<p>Wenn Sie einen Sender im DAPNET eintragen lassen, stimmen Sie freiwillig zu, dass die von Ihnen
+				bereitgestellten Daten des Senders (insb. die Position, die Höhe und die Ausgangsleistung) auf dieser
+				Webseite oder in anderen Netzwerken oder Plattformen (z.B. das APRS-Netzwerk im Amateurfunk) verarbeitet
+				und/oder veröffentlicht werden. Dies wird als bestimmungsgemäßer Betrieb definiert. Diese Definition
+				wird von Ihnen ausdrücklich akzeptiert.
+			</p>
+
+		</v-card-text>
+	</v-card>
+</template>
+
+<script>
+    export default {
+        name: "privacy"
+    }
+</script>
+
+<style scoped>
+
+</style>
